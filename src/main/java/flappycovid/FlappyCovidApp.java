a83f7a09a78e747b2a8d679df980d3b4656fa0e5
wulffZ <mattje74@gmail.com>
2022-04-05 12:43:41 +0200

    initial commit

---
+++ src/main/java/flappycovid/FlappyCovidApp.java
@@ -0,0 +1,16 @@
+package flappycovid;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class FlappyCovidApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
